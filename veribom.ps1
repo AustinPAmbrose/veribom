e750d18ccc573d690fc756c9f9519df34cf3fdf5
apambrose <austin.p.ambrose@gmail.com>
2023-05-18 21:42:06 -0700

    fixed a bug in veribom updater

--- veribom.ps1
+++ veribom.ps1
@@ -72,7 +72,7 @@ function check_for_updates {
             try {
                 $ProgressPreference = "SilentlyContinue"
                 Invoke-WebRequest "https://github.com/AustinPAmbrose/veribom/raw/main/release.zip" -OutFile "$home\downloads\veribom_temp.zip"
-                Remove-Item "$home\downloads\veribom_temp" -Recurse -Force
+                Remove-Item "$home\downloads\veribom_temp" -Recurse -Force -ErrorAction SilentlyContinue
                 Expand-Archive "$home\downloads\veribom_temp.zip" -DestinationPath "$home\downloads\veribom_temp" -Force
                 Remove-Item "$home\downloads\veribom_temp.zip"
                 return (Test-ScriptFileInfo "$home\downloads\veribom_temp\veribom.ps1").Version
